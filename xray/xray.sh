fefc089be1a77ba1d4c9993441ada966308e05e1
Pavel <p30534437@gmail.com>
2026-06-11 13:35:35 +0600

    целая гора исправлений от подруги

--- xray/xray.sh
+++ xray/xray.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
 pkill xray
-sleep 0.5
-
-REAL_CONFIG=$(readlink -f ~/.config/xray/config.json)
-CONFIG_NAME=$(basename "$REAL_CONFIG")
-
-zenity --notification --text="$CONFIG_NAME" &
 
 /usr/bin/xray run -c ~/.config/xray/config.json
